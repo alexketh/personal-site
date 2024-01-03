7d6bb4c176b5850915fc0d8f55c554d8b9021b59
alexketh <alexketh.ca@gmail.com>
2024-01-02 17:07:03 -0700

    Changed URL, PFP, Sidebar Info

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -12,18 +12,18 @@ const SideBar = () => (
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael@mldangelo.com">michael@mldangelo.com</a></p>
+        <h2>Alex Keth</h2>
+        <p><a href="mailto:alexketh@gmail.com">alexketh@gmail.com</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni,
-        and the VP of Engineering at <a href="https://smileidentity.com">Smile Identity</a>. Previously,
-        I was the co-founder and CTO of <a href="https://arthena.com">Arthena</a>
-        , co-founder of <a href="https://matroid.com">Matroid</a>, and worked at
-        {' '}<a href="https://planet.com">Planet</a> and <a href="https://facebook.com">Facebook</a>.
+      <p>Hi, I&apos;m Alex. I am a CSUS.
+        I am a <a href="https://catalog.csus.edu/colleges/engineering-computer-science/computer-science/bs-in-computer-science/">CSUS Computer Science</a> Graduate.
+        Previously, I was a Software Enginner at <a href="https://www.carnegiehighered.com/fire-engine-red-student-search/">Fire Engine RED</a>.
+        My focus is primarily in front end development,
+        but I am open to develop in any part of the stack.
       </p>
       <ul className="actions">
         <li>
@@ -34,7 +34,7 @@ const SideBar = () => (
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; Alex Keth <Link to="/">https://alexketh.github.io/personal-site/</Link>.</p>
     </section>
   </section>
 );
